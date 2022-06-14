8a797c75bc0722fa28a1dab20ab36a55e715ff9d
unknown <reyhanzelvian007@gmail.com>
2022-06-14 21:02:14 +0700

    feat: adding NavbarNotHome in components

---
+++ src/components/NavbarNotHome/NavbarNotHome.jsx
@@ -0,0 +1,25 @@
+import style from './NavbarNotHome.module.css';
+import Logo from '../Logo/logo';
+
+const NavbarNotHome = () => {
+  return (
+    <>
+        <nav>
+            <div className={style.leftNavbar}>
+                <Logo />
+                <div className={style.inputContainer}>
+                    <input type="search" placeholder="Cari di sini ..." />
+                    <img src="icons/fi_search.svg" alt="search" />
+                </div>
+            </div>
+            <div className={style.rightNavbar}>
+                <img src="/icons/fi_list.svg" alt="list" />
+                <img src="/icons/fi_bell.svg" alt="bell" />
+                <img src="/icons/fi_user.svg" alt="user" />
+            </div>
+        </nav>
+    </>
+  )
+}
+
+export default NavbarNotHome;
