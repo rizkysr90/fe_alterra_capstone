1368bdb99c89a0fa6a4bc9a802fa1668fb716b02
unknown <reyhanzelvian007@gmail.com>
2022-06-14 20:37:16 +0700

    feat: adding Home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,13 +1,65 @@
-import Logo from '../../components/Logo/logo';
-import Card from "../../components/Card/Card"
+import Card from "../../components/Card/Card";
 import NavbarHome from '../../components/NavbarHome/NavbarHome';
+import style from './Home.module.css';
 
 const Home = () => {
   return (
     <>
+      <div className={style.homeContainer}>
         <NavbarHome />
-        <Logo />
-        <Card />
+        <div className={style.heroContainer}>
+          <div className={style.leftBox}></div>
+          <div className={style.middleBox}>
+            <div className={style.heroDesc}>
+              <h1>Bulan Ramadhan Banyak diskon!</h1>
+              <h5>Diskon Hingga</h5>
+              <h3>60%</h3>
+            </div>
+            <img 
+              src="images/png_gift_88837.png" 
+              alt="gift"
+              className={style.giftImage}/>
+            <img
+              src="images/background_hero.png"
+              alt="background"
+              className={style.backgroundImage}/>
+          </div>
+          <div className={style.rightBox}></div>
+        </div>
+        <div className={style.categoryContainer}>
+          <h2>Telusuri Kategori</h2>
+          <div className={style.categoryButtonContainer}>
+            <button>Semua</button>
+            <button>Hobi</button>
+            <button>Kendaraan</button>
+            <button>Baju</button>
+            <button>Elektronik</button>
+            <button>Kesehatan</button>
+          </div>
+        </div>
+        <div className={style.cardContainer}>
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+        </div>
+      </div>
     </>
   )
 }
