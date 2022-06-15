46071fe0be6e77a1e5b282b2a1178b632555917e
Angelia Christy <angeliachrst@gmail.com>
2022-06-15 15:32:10 +0700

    feat: first commit page daftar jual

--- src/pages/DaftarJual/DaftarJual.jsx
+++ src/pages/DaftarJual/DaftarJual.jsx
@@ -1,7 +1,18 @@
+import Navbar from '../../components/NavbarNotHome/NavbarNotHome';
+import style from './DaftarJual.module.css';
+
 const DaftarJual = () => {
   return (
     <>
-        <h1>Daftar Jual</h1>
+        <Navbar />
+        <div className={style.container}>
+          <h1 className={style.titlePage}>Daftar Jual Saya</h1>
+          <div className={style.boxProfile}>
+            <div className={style.profileContent}></div>
+            <div className={style.profileContent}></div>
+            <div className={style.profileContent}></div>
+          </div>
+        </div>
     </>
   )
 }
