64d231883453ddbfa40bc9a64ecfe2e9673905e8
Richard99-ux <kancilcakep99@gmail.com>
2022-06-17 15:43:16 +0700

    feat: fix halaman-product-seller

--- src/pages/SellerProduct/SellerProduct.jsx
+++ src/pages/SellerProduct/SellerProduct.jsx
@@ -1,7 +1,70 @@
+import NavbarNotHome from "../../components/NavbarNotHome/NavbarNotHome";
+import style from "./SellerProduct.module.css";
+import { Carousel } from 'react-bootstrap';
+
+
+
 const SellerProduct = () => {
   return (
     <>
-        <h1>Halaman Produk Seller</h1>
+      <NavbarNotHome />
+
+      <Carousel className={style.use}>
+        <Carousel.Item>
+          <img
+            className=""
+            src="/images/watch.png"
+            alt="First slide"
+          />
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className=""
+            src="/images/watch.png"
+            alt="Second slide"
+          />
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block"
+            src="/images/watch.png"
+            alt="Third slide"
+          />
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block"
+            src="/images/watch.png"
+            alt="Four slide"
+          />
+        </Carousel.Item>
+      </Carousel>
+
+      <div className={style.card}>
+        <h5 className={style.tha}>Jam Tangan Casio</h5>
+        <p className={style.tri}>Aksesoris</p>
+        <h5 className={style.pro}>Rp 250.000</h5>
+        <button className={style.sob}>Terbitkan</button>
+        <button className={style.man}>Edit</button>
+      </div>
+
+      <div className={style.cardPenjual}>
+        <div className={style.par}>
+          <img src="/images/profilPenjual.png" alt="Foto Penjual" />
+          <div className={style.tup}>
+            <h5 className={style.dea}>Nama Penjual</h5>
+            <p className={style.sun}>Kota</p>
+          </div>
+        </div>
+      </div>
+
+      <div className={style.cardContainer}>
+        <h5 className={style.kri}>Deskripsi</h5>
+        <div className={style.ips}>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        </div>
+      </div>
     </>
   )
 }
