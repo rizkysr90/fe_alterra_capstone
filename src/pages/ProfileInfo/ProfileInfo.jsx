e72c02a185f46aead6d25a9ae919f9a255d053d8
Richard99-ux <kancilcakep99@gmail.com>
2022-06-22 11:07:52 +0700

    feat: fix pages

--- src/pages/ProfileInfo/ProfileInfo.jsx
+++ src/pages/ProfileInfo/ProfileInfo.jsx
@@ -6,7 +6,7 @@ const ProfileInfo = () => {
 
   return (
     <>
-      <Navbar title="Lengkapi Info Akun"/>
+      <Navbar title="Lengkapi Info Akun"/> 
       <div className={style.container}>
         <div className={style.content}>
           <img src="/icons/arrow-left.svg" alt="Icon Back" />
