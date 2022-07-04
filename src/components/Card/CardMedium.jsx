56919c05fb65e06b1f2f7b4259e2c653ea54f1d9
Angelia Christy <angeliachrst@gmail.com>
2022-07-04 23:27:06 +0700

    feat: add Link to get detail product

--- src/components/Card/CardMedium.jsx
+++ src/components/Card/CardMedium.jsx
@@ -1,15 +1,18 @@
 import style from "./CardMedium.module.css";
+import { Link } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import { sellerAction } from "../../config/redux/actions/sellerAction";
 import { useEffect } from "react";
 
 const CardMedium = () => {
-  const { dataProductSeller } = useSelector((globalStore) => globalStore.sellerReducer);
+  const { dataProductSeller } = useSelector(
+    (globalStore) => globalStore.sellerReducer
+  );
   console.log(dataProductSeller);
 
   const dispatch = useDispatch();
   const { dataLogin } = useSelector((state) => state.auth);
-  const token = `${dataLogin.dataLogin.token}`
+  const token = `${dataLogin.dataLogin.token}`;
 
   const rupiah = (number) => {
     return new Intl.NumberFormat("id-ID", {
@@ -27,7 +30,9 @@ const CardMedium = () => {
     <>
       {dataProductSeller?.map((products) => (
         <div key={products.id} className={style.cardContainer}>
-          <img src={products.Product_images[0].url_image} alt="card" />
+          <Link to={`/seller-product/${products.id}`}>
+            <img src={products.Product_images[0].url_image} alt="card" />
+          </Link>
           <div className={style.cardDesc}>
             <h5>{`${products.name.slice(0, 15)}...`}</h5>
             <p>{products.Category.name}</p>
