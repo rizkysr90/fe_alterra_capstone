dec309a402d53987a2aafec135ac169f652608dd
unknown <reyhanzelvian007@gmail.com>
2022-07-06 20:29:22 +0700

    fix: styling

--- src/components/Logo/logo.jsx
+++ src/components/Logo/logo.jsx
@@ -1,8 +1,15 @@
 import style from './logo.module.css';
+import { useNavigate } from 'react-router-dom';
 
 const Logo = () => {
+    const navigate = useNavigate();
+
+    const handleNavigateHome = () => {
+        navigate("/");
+    };
+
     return (
-        <div className={style.logo}>
+        <div className={style.logo} onClick={() => handleNavigateHome()}>
             <h1 className={style.logoTitle}>SecondHand.</h1>
         </div>
     );
