cac575c14958e227ff89f7f979f412d314ab7798
Angelia Christy <angeliachrst@gmail.com>
2022-07-18 21:59:59 +0700

    feat: try to format date and time at page Notifikasi

--- src/pages/Notifikasi/Notifikasi.jsx
+++ src/pages/Notifikasi/Notifikasi.jsx
@@ -8,108 +8,108 @@ import { useSelector } from "react-redux";
 import { useState } from "react";
 
 const Notifikasi = () => {
-	const { dataLogin } = useSelector((globalStore) => globalStore.auth);
-	const [dataNotif, setDataNotif] = useState([]);
+  const { dataLogin } = useSelector((globalStore) => globalStore.auth);
+  const [dataNotif, setDataNotif] = useState([]);
 
-	const getNotif = async () => {
-		//eslint-disable-next-line
-		const { data } = await axios({
-			method: "get",
-			url: "https://secondhand-apibejs2-staging.herokuapp.com/api/v1.0/notifications",
-			headers: {
-				Authorization: `Bearer ${dataLogin.dataLogin.token}`,
-			},
-		});
-		setDataNotif(data.data);
+  const getNotif = async () => {
+    //eslint-disable-next-line
+    const { data } = await axios({
+      method: "get",
+      url: "https://secondhand-apibejs2-staging.herokuapp.com/api/v1.0/notifications",
+      headers: {
+        Authorization: `Bearer ${dataLogin.dataLogin.token}`,
+      },
+    });
+    setDataNotif(data.data);
     console.log(data.data);
-	};
+  };
 
-	const checkPenawaran = (data) => {
-		return data.Notification_object.Notification_type.id === 2;
-	};
+  const checkPenawaran = (data) => {
+    return data.Notification_object.Notification_type.id === 2;
+  };
 
-	const checkPenawaranDiterima = (data) => {
-		return data.Notification_object.Notification_type.id === 3;
-	};
+  const checkPenawaranDiterima = (data) => {
+    return data.Notification_object.Notification_type.id === 3;
+  };
 
-	const dataPenawaran = dataNotif.filter(checkPenawaran);
-	const dataPenawaranDiterima = dataNotif.filter(checkPenawaranDiterima);
-	console.log(dataNotif);
+  const dataPenawaran = dataNotif.filter(checkPenawaran);
+  const dataPenawaranDiterima = dataNotif.filter(checkPenawaranDiterima);
+  console.log(dataNotif);
 
-	const rupiah = (number) => {
-		return new Intl.NumberFormat("id-ID", {
-			style: "currency",
-			currency: "IDR",
-		}).format(number);
-	};
+  const rupiah = (number) => {
+    return new Intl.NumberFormat("id-ID", {
+      style: "currency",
+      currency: "IDR",
+    }).format(number);
+  };
 
-	useEffect(() => {
-		getNotif();
-		document.getElementsByClassName(NavbarStyle.iconBell)[0].style.stroke =
-			"#7126B5";
-		//eslint-disable-next-line
-	}, []);
+  useEffect(() => {
+    getNotif();
+    document.getElementsByClassName(NavbarStyle.iconBell)[0].style.stroke =
+      "#7126B5";
+    //eslint-disable-next-line
+  }, []);
 
-	return (
-		<>
-			<Navbar />
-			<Sidebar />
-			<div className={style.container}>
-				<div className={style.content}>
-					<h1 className={style.titleRes}>Notifikasi</h1>
-					{dataPenawaran.map((data) => (
-						<div className={style.cardNotifikasi}>
-							<div className={style.cardContent}>
-								<img
-									className={style.imgNotifikasi}
-									src="/images/watch.png"
-									alt="Foto Produk"
-								/>
-							</div>
-							<div className={style.cardContent}>
-								<p>Penawaran produk</p>
-								<h2>{data.Notification_object.Order.Product.name}</h2>
-								<h2>{`${rupiah(data.Notification_object.Order.price)}`}</h2>
-								<h2>
-									Ditawar{" "}
-									{`${rupiah(data.Notification_object.Order.Product.price)}`}
-								</h2>
-							</div>
-							<div className={style.cardContent}>
-								<p>{data.createdAt}</p>
-								<div className={style.elipse}></div>
-							</div>
-						</div>
-					))}
-					{dataPenawaranDiterima.map((data) => (
-						<div className={style.cardNotifikasi}>
-							<div className={style.cardContent}>
-								<img
-									className={style.imgNotifikasi}
-									src="/images/watch.png"
-									alt="Foto Produk"
-								/>
-							</div>
-							<div className={style.cardContent}>
-								<p>Penawaran produk</p>
-								<h2>{data.Notification_object.Order.Product.name}</h2>
-								<h2>{`${rupiah(data.Notification_object.Order.price)}`}</h2>
-								<h2>
-									Berhasil Ditawar{" "}
-									{`${rupiah(data.Notification_object.Order.Product.price)}`}
-								</h2>
+  return (
+    <>
+      <Navbar />
+      <Sidebar />
+      <div className={style.container}>
+        <div className={style.content}>
+          <h1 className={style.titleRes}>Notifikasi</h1>
+          {dataPenawaran.map((data) => (
+            <div className={style.cardNotifikasi}>
+              <div className={style.cardContent}>
+                <img
+                  className={style.imgNotifikasi}
+                  src="/images/watch.png"
+                  alt="Foto Produk"
+                />
+              </div>
+              <div className={style.cardContent}>
+                <p>Penawaran produk</p>
+                <h2>{data.Notification_object.Order.Product.name}</h2>
+                <h2>{`${rupiah(data.Notification_object.Order.price)}`}</h2>
+                <h2>
+                  Ditawar{" "}
+                  {`${rupiah(data.Notification_object.Order.Product.price)}`}
+                </h2>
+              </div>
+              <div className={style.cardContent}>
+                <p>{data.createdAt}</p>
+                <div className={style.elipse}></div>
+              </div>
+            </div>
+          ))}
+          {dataPenawaranDiterima.map((data) => (
+            <div className={style.cardNotifikasi}>
+              <div className={style.cardContent}>
+                <img
+                  className={style.imgNotifikasi}
+                  src="/images/watch.png"
+                  alt="Foto Produk"
+                />
+              </div>
+              <div className={style.cardContent}>
+                <p>Penawaran produk</p>
+                <h2>{data.Notification_object.Order.Product.name}</h2>
+                <h2>{`${rupiah(data.Notification_object.Order.price)}`}</h2>
+                <h2>
+                  Berhasil Ditawar{" "}
+                  {`${rupiah(data.Notification_object.Order.Product.price)}`}
+                </h2>
                 <p>Kamu akan segera dihubungi penjual via whatsapp</p>
-							</div>
-							<div className={style.cardContent}>
-								<p>{data.createdAt}</p>
-								<div className={style.elipse}></div>
-							</div>
-						</div>
-					))}
-				</div>
-			</div>
-		</>
-	);
+              </div>
+              <div className={style.cardContent}>
+                <p>{data.createdAt}</p>
+                <div className={style.elipse}></div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Notifikasi;
