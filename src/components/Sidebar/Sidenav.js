e3243332d6486ab3c36505c0e947f537c7d89233
Angelia Christy <angeliachrst@gmail.com>
2022-07-18 21:26:40 +0700

    feat: fix urutan sidebar

--- src/components/Sidebar/Sidenav.js
+++ src/components/Sidebar/Sidenav.js
@@ -10,14 +10,14 @@ const Sidenav = ({ sidenav }) => {
             <Link to={"/"}>
                 <p><b>Second Hand</b></p>
             </Link>
+            <Link to={"/daftar-jual"}>
+                <li>Daftar Jual</li>
+            </Link>
             <Link to={"/notifikasi"}>
                 <li>Notifikasi</li>
             </Link>
             <Link to={"/riwayat-pembelian"}>
                 <li>Riwayat Pembelian</li>
-            </Link>
-            <Link to={"/daftar-jual"}>
-                <li>Daftar Jual</li>
             </Link>
             <Link to={"/profile"}>
                 <li>Akun Saya</li>
