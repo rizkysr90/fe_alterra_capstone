06c5b463d5db3b18e6a860dcbd51fe5bde94bad5
Angelia Christy <angeliachrst@gmail.com>
2022-07-18 17:58:38 +0700

    feat: undo login test

--- src/testing/Login.test.js
+++ src/testing/Login.test.js
@@ -1,27 +1,27 @@
-// import { render, screen } from '@testing-library/react';
-// // import userEvent from '@testing-library/user-event';
-// import Login from '../pages/Login/Login';
-// import { Provider } from "react-redux";
-// import configureStore from "redux-mock-store";
+import { render, screen } from '@testing-library/react';
+// import userEvent from '@testing-library/user-event';
+import Login from '../pages/Login/Login';
+import { Provider } from "react-redux";
+import configureStore from "redux-mock-store";
 
-// describe('Test Login Page', () => {
-//     const initialState = { dataLogin: null }
-//     const mockConfigStore = configureStore();
-//     const mockStore = mockConfigStore(initialState);
-//     const MOCK_FUNCTION = jest.fn();
+describe('Test Login Page', () => {
+    const initialState = { dataLogin: null }
+    const mockConfigStore = configureStore();
+    const mockStore = mockConfigStore(initialState);
+    const MOCK_FUNCTION = jest.fn();
 
-//     beforeEach(() => {
-//         //eslint-disable-next-line
-//         render(
-//             <>
-//                 <Provider store={mockStore}>
-//                     <Login CALL_FUNCTION={MOCK_FUNCTION} />
-//                 </Provider>
-//             </>
-//         )
-//     });
-//     test('cek apakah ada text heading', () => {
-//         const textHeading = screen.getByTestId('title');
-//         expect(textHeading).toBeInTheDocument();
-//     });
-// })
+    beforeEach(() => {
+        //eslint-disable-next-line
+        render(
+            <>
+                <Provider store={mockStore}>
+                    <Login CALL_FUNCTION={MOCK_FUNCTION} />
+                </Provider>
+            </>
+        )
+    });
+    test('cek apakah ada text heading', () => {
+        const textHeading = screen.getByTestId('title');
+        expect(textHeading).toBeInTheDocument();
+    });
+})
